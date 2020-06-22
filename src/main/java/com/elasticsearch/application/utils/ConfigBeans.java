541873e88def29c31084127e6a0a882f96299092
1414524230@qq.com <1414524230@qq.com>
2020-06-22 15:13:33 +0800

    添加注释,提取url

--- src/main/java/com/elasticsearch/application/utils/ConfigBeans.java
+++ src/main/java/com/elasticsearch/application/utils/ConfigBeans.java
@@ -1,16 +1,18 @@
 package com.elasticsearch.application.utils;
 
 
-import com.elasticsearch.application.User;
 import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigBeans {
 
+    @Value("${elasticsearch.connection.url}")
+    private String URL;
 
     /**
      * 生成 search 连接客户端
@@ -20,7 +22,7 @@ public class ConfigBeans {
     public JestClient getJestClient(){
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(
-                new HttpClientConfig.Builder("http://localhost:9200")
+                new HttpClientConfig.Builder(URL)
                         .multiThreaded(true)
                         .defaultMaxTotalConnectionPerRoute(2)
                         .maxTotalConnection(10)
